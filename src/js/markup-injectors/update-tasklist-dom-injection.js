853879e710f7109d3aae647f34f6bf60cb44980a
iturres <emanueliturres.co@gmail.com>
2023-06-07 21:30:33 -0300

    Update 'updateTaskListOnDom' -function- to handle markup injection

--- src/js/markup-injectors/update-tasklist-dom-injection.js
+++ src/js/markup-injectors/update-tasklist-dom-injection.js
@@ -1,12 +1,18 @@
 import TaskManager from '../user-controller/task-manager-class.js';
 import createTaskTemplate from '../markup-templates/create-task-template.js';
 
-const updateTaskListOnDOM = () => {
+const updateTaskListOnDOM = (updateWhen) => {
   const taskListHolder = document.querySelector('[data-task-list-holder]');
   const taskList = new TaskManager().getTaskList();
-  taskList.forEach((task) => {
-    taskListHolder.innerHTML += createTaskTemplate(task);
-  });
+  if (updateWhen === 'lastTask') {
+    taskListHolder.innerHTML += createTaskTemplate(
+      taskList[taskList.length - 1]
+    );
+  } else if (updateWhen === 'fullList') {
+    taskList.forEach((task) => {
+      taskListHolder.innerHTML += createTaskTemplate(task);
+    });
+  }
 };
 
 export default updateTaskListOnDOM;
