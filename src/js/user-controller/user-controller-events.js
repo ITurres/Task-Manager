6efb6d7876bb30603c5c7ad2b671440d8c3715af
iturres <emanueliturres.co@gmail.com>
2023-06-08 15:14:13 -0300

    Remove/Add - Remove 'deletion of each task event' - Add 'task checkbox check event'

--- src/js/user-controller/user-controller-events.js
+++ src/js/user-controller/user-controller-events.js
@@ -1,9 +1,11 @@
 import updateTaskListOnDOM from '../markup-injectors/update-tasklist-dom-injection.js';
-import getParentElementId from '../utils/get-parent-element-id.js';
+import getParentElement from '../utils/get-parent-element-id.js';
 import TaskManager from './task-manager-class.js';
+import updateCompletedTaskStyles from '../utils/updateCompletedTaskStyles.js';
 
 if (window.localStorage.length > 0) {
   updateTaskListOnDOM('fullList');
+  updateCompletedTaskStyles();
 }
 
 document.querySelector('[data-refresh-btn]').addEventListener('click', () => {
@@ -21,15 +23,6 @@ document.querySelector('[data-add-task]').addEventListener('click', () => {
   window.location.reload();
 });
 
-document.querySelectorAll('[data-delete-task]').forEach((button) => {
-  button.addEventListener('click', () => {
-    const parentDivId = getParentElementId(button, 'div');
-    const taskList = new TaskManager();
-    taskList.removeTask(parentDivId);
-    window.location.reload();
-  });
-});
-
 document.querySelectorAll('[data-task]').forEach((task) => {
   task.addEventListener('click', () => {
     task.readOnly = false;
@@ -38,9 +31,20 @@ document.querySelectorAll('[data-task]').forEach((task) => {
   document.addEventListener('click', (event) => {
     // ? if user has clicked outside the input => update data
     if (!task.contains(event.target)) {
-      const parentDivId = getParentElementId(task, 'div');
+      const parentDiv = getParentElement(task, 'div');
       const taskList = new TaskManager();
-      taskList.updateTask({ index: parentDivId, newDescription: task.value });
+      taskList.updateTask({ index: parentDiv.id, description: task.value });
     }
+  });
+});
+
+document.querySelectorAll('[data-task-checkbox]').forEach((checkbox) => {
+  checkbox.addEventListener('change', (event) => {
+    const taskCompleted = JSON.parse(event.target.dataset.completed);
+    const parentDiv = getParentElement(checkbox, 'div');
+    const taskList = new TaskManager();
+    taskList.updateTask({ index: parentDiv.id, completed: !taskCompleted });
+    parentDiv.classList.toggle('task-completed');
+    window.location.reload();
   });
 });
