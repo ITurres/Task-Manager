b8a77782aa234776fda6b84f2dc57305f2564b81
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-06-14 22:31:33 -0600

    Add new element to DOM function test

--- src/Jest/TaskManager.test.js
+++ src/Jest/TaskManager.test.js
@@ -3,6 +3,7 @@
  */
 
 const TaskManager = require('./TaskManager-toTest.js');
+const injectLastTask = require('./addNewTasktoDOM.js');
 
 describe('TaskManager - add New Task to LocalStorage', () => {
   let taskList;
@@ -25,5 +26,27 @@ describe('TaskManager - add New Task to LocalStorage', () => {
     taskList.addNewTask({ description: newTaskDescription });
 
     expect(taskList.getTaskList().length).toBe(1);
+  });
+});
+
+describe('TaskManager - add a new element to the DOM', () => {
+  // let taskList;
+
+  beforeEach(() => {
+    // taskList = new TaskManager();
+    document.body.innerHTML = '<div data-task-list-holder></div>';
+  });
+
+  afterEach(() => {
+    document.body.innerHTML = '';
+    // taskList = null;
+  });
+
+  test('Should add a new DOM elemnt after adding a new tasklist', () => {
+    const arrayToDoList = document.querySelectorAll(
+      '[data-task-list-holder] div'
+    );
+    injectLastTask();
+    expect(arrayToDoList).toHaveLength(1);
   });
 });
---
+++ src/Jest/addNewTasktoDOM.js
@@ -0,0 +1,10 @@
+import TaskManager from '../js/user-controller/task-manager-class.js';
+import createTaskTemplate from '../js/markup-templates/create-task-template.js';
+
+const injectLastTask = () => {
+  const taskListHolder = document.querySelector('[data-task-list-holder]');
+  const taskList = new TaskManager().getTaskList();
+  taskListHolder.innerHTML += createTaskTemplate(taskList[taskList.length - 1]);
+};
+
+module.exports = injectLastTask;
