9744f3f4cad5aac962b6e5fbbf48aaa4f4643f67
iturres <emanueliturres.co@gmail.com>
2023-06-15 16:51:06 -0300

    Modify 'injectLastTask' -function-

--- src/Jest/addNewTasktoDOM.js
+++ src/Jest/addNewTasktoDOM.js
@@ -2,12 +2,8 @@
 import createTaskTemplate from '../js/markup-templates/create-task-template.js';
 
 const injectLastTask = (taskListHolder, taskList) => {
-  // // const taskListHolder = document.querySelector('[data-task-list-holder]');
-
-  // const taskList = new TaskManager().getTaskList();
   const lastTask = taskList[taskList.length - 1];
-  const taskElement = createTaskTemplate(lastTask);
-  taskListHolder.appendChild(taskElement);
+  taskListHolder.innerHTML += createTaskTemplate(lastTask);
 };
 
 module.exports = injectLastTask;
