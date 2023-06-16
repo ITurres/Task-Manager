bcc6cf3bf74fb2aa5bf608c7d5a207f7c14c223a
iturres <emanueliturres.co@gmail.com>
2023-06-15 21:47:48 -0300

    Add 'task description edition' -test-

---
+++ src/Jest/TaskManager-edit-update-complete.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const TaskManager = require('./TaskManager-toTest.js');
+// const injectFullTaskList = require('./injectFullTaskListOnDOM.js');
+
+describe('TaskManager - Edit task description on LocalStorage', () => {
+  let taskList;
+
+  beforeEach(() => {
+    taskList = new TaskManager();
+  });
+
+  afterEach(() => {
+    taskList = null;
+    localStorage.clear();
+  });
+
+  test('Should update an element description of the taskList array', () => {
+    taskList.addNewTask({ description: 'New Task Test 5' });
+
+    taskList.updateTask({ index: 0, description: 'Updated Task Test 5' });
+
+    expect(taskList.getTaskList()[0].description).toBe('Updated Task Test 5');
+  });
+});
