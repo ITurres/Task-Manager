c6951e3cc0d624926277791b9edb98ecb43d7480
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-06-15 18:59:49 -0600

    Add update task completed Property -Test-

--- src/Jest/TaskManager-edit-update-complete.test.js
+++ src/Jest/TaskManager-edit-update-complete.test.js
@@ -25,3 +25,24 @@ describe('TaskManager - Edit task description on LocalStorage', () => {
     expect(taskList.getTaskList()[0].description).toBe('Updated Task Test 5');
   });
 });
+
+describe('TaskManager - Update task completed property on LocalStorage', () => {
+  let taskList;
+
+  beforeEach(() => {
+    taskList = new TaskManager();
+  });
+
+  afterEach(() => {
+    taskList = null;
+    localStorage.clear();
+  });
+
+  test('Should update an element completed property', () => {
+    taskList.addNewTask({ description: 'New Task Test 6' });
+
+    taskList.updateTask({ index: 0, completed: true });
+
+    expect(taskList.getTaskList()[0].completed).toBeTruthy();
+  });
+});
